f17697a6f9492050b8bbb26e5adb32436f897fb6
Neous1 <Neous1@users.noreply.github.com>
2017-12-11 06:33:23 -0500

    initial commit

---
+++ BasicDelegates/BasicDelegates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicDelegates
+{
+    //declare the delegate type
+    public delegate string MyDelegate(int arg1, int arg2);
+
+    class Program
+    {
+        static string func1(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+        static string func2(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
