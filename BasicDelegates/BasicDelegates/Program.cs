8d0d05e9e6b70b2b9bbbaf99e549c5a513bf0d3b
Neous1 <Neous1@users.noreply.github.com>
2017-12-11 09:05:32 -0500

    create class myClass in Main() create an object mc of MyClass assign mc to delegate f and call and inherit class method

--- BasicDelegates/BasicDelegates/Program.cs
+++ BasicDelegates/BasicDelegates/Program.cs
@@ -8,7 +8,16 @@ namespace BasicDelegates
 {
     //declare the delegate type
     public delegate string MyDelegate(int arg1, int arg2);
-    
+
+    class MyClass
+    {
+        // Delegates can be bound to instance members as well as
+        // static class functions
+        public string InstanceMethod1(int arg1, int arg2)
+        {
+            return ((arg1 + arg2) * arg1).ToString();
+        }
+    }
     class Program
     {
         static string Func1(int a, int b)
@@ -25,6 +34,12 @@ namespace BasicDelegates
             Console.WriteLine("The number is: " + f(10, 20));
             f = Func2;
             Console.WriteLine("The number is: " + f(10, 20));
+
+            MyClass mc = new MyClass();
+            f = mc.InstanceMethod1;
+            Console.WriteLine("The number is: " + f(10, 20));
+
+            Console.WriteLine();
         }
     }
 }
